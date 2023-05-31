1e2581834cd842809bbb087130db386136b25014
mjgutierre <mjgutierre@eafit.edu.co>
2023-05-31 12:35:52 -0500

    añadi codigos,datasets de acciones y probe local

--- Reto5/MapReduce/Acciones/b_estable_mr.py
+++ Reto5/MapReduce/Acciones/b_estable_mr.py
@@ -1,2 +1,30 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
+
+class EstableAltoStock(MRJob):
+
+    def mapper(self, _, line):
+        fields = line.split(',')
+        
+        # Extrae el nombre de la empresa y el precio de la línea
+        company = fields[0]
+        price = float(fields[1])
+        
+        # nombre de la empresa como clave y precio como valor
+        yield company, price
+
+    def reducer(self, company, prices):
+        price_list = list(prices)
+        
+        # Verifica si los precios siempre han subido o se han mantenido estables
+        if all(price_list[i] <= price_list[i+1] for i in range(len(price_list)-1)):
+            # Emite el nombre de la empresa como resultado final
+            yield company, None
+
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper, reducer=self.reducer)
+        ]
+
+if __name__ == '__main__':
+    EstableAltoStock.run()
--- Reto5/MapReduce/Acciones/c_dianegro_mr.py
+++ Reto5/MapReduce/Acciones/c_dianegro_mr.py
@@ -1,2 +1,38 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
+
+class BlackDayMR(MRJob):
+
+    def mapper(self, _, line):
+        fields = line.split(',')
+        
+        # Extrae el nombre de la empresa, el precio y la fecha de la línea
+        company = fields[0]
+        price = float(fields[1])
+        date = fields[2]
+        
+        # día como clave y una tupla (empresa, precio) como valor
+        yield date, (company, price)
+
+    def reducer(self, date, values):
+        # Inicializa una lista para almacenar las acciones con el menor valor de acción
+        min_price_companies = []
+        min_price = float('inf')
+        
+        # Encuentra el valor de acción más bajo para cada día y guarda las acciones correspondientes
+        for company, price in values:
+            if price < min_price:
+                min_price = price
+                min_price_companies = [company]
+            elif price == min_price:
+                min_price_companies.append(company)
+        
+        yield date, min_price_companies
+
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper, reducer=self.reducer)
+        ]
+
+if __name__ == '__main__':
+    BlackDayMR.run()
